04db5ef8070c9f4b249542284c64efaed83821ba
psududemike <psududemike@gmail.com>
2015-11-23 21:15:33 -0500

    Update arm_demo.c

--- arm_demo.c
+++ arm_demo.c
@@ -5,6 +5,11 @@
 #define lowtri D3
 #define elbow1 D5
 #define elbow2 D1
+#define Lhand A5
+#define Lwrist A4
+#define Ltri1 A3
+#define Ltri2 A2
+#define Lelbow A1
 
 
 void setup() {
@@ -15,6 +20,11 @@ void setup() {
     pinMode(lowtri,OUTPUT);
     pinMode(elbow1,OUTPUT);
     pinMode(elbow2,OUTPUT);
+    pinMode(Lhand,OUTPUT);
+    pinMode(Lwrist,OUTPUT);
+    pinMode(Ltri1,OUTPUT);
+    pinMode(Ltri2,OUTPUT);
+    pinMode(Lelbow,OUTPUT);
     Particle.function("function",picker);
     Particle.function("zone",activateZone);
 }
@@ -58,24 +68,40 @@ int activateZone(String command)
     Serial.printf("Activate %s for %d s in %d s",zone.c_str(),length,offset);
     
     delay(offset);
-    if(zone=="hand"){
+    if(zone=="Rhand"){
         digitalWrite(hand1,HIGH);
         digitalWrite(hand2,HIGH);
         delay(length);
         digitalWrite(hand1,LOW);
         digitalWrite(hand2,LOW);
-    } else if(zone=="tricep"){
+    } else if(zone=="Rtricep"){
         digitalWrite(tricep1,HIGH);
         digitalWrite(tricep2,HIGH);
         delay(length);
         digitalWrite(tricep1,LOW);
         digitalWrite(tricep2,LOW);
-    } else if(zone=="elbow"){
+    } else if(zone=="Relbow"){
         digitalWrite(elbow1,HIGH);
         digitalWrite(elbow2,HIGH);
         delay(length);
         digitalWrite(elbow1,LOW);
         digitalWrite(elbow2,LOW);
+    } if(zone=="Lhand"){
+        digitalWrite(Lhand,HIGH);
+        delay(length);
+        digitalWrite(Lhand,LOW);
+    } else if(zone=="Ltricep"){
+        digitalWrite(Ltri1,HIGH);
+        delay(length);
+        digitalWrite(Ltri1,LOW);
+    } else if(zone=="Lelbow"){
+        digitalWrite(Lelbow,HIGH);
+        delay(length);
+        digitalWrite(Lelbow,LOW);
+    } else if (zone=="Lwrist") {
+        digitalWrite(Lwrist,HIGH);
+        delay(length);
+        digitalWrite(Lwrist,LOW);
     }
         
     
@@ -101,6 +127,27 @@ void doCreepy(){
     delay(200);
     digitalWrite(elbow1,LOW);
     digitalWrite(elbow2,LOW);
+    delay(20);
+    digitalWrite(Lelbow,HIGH);
+    delay(200);
+    digitalWrite(Lelbow,LOW);
+    delay(20);
+    // digitalWrite(Ltri2,HIGH);
+    // delay(200);
+    // digitalWrite(Ltri2,LOW);
+    // delay(20);
+    digitalWrite(Ltri1,HIGH);
+    delay(200);
+    digitalWrite(Ltri1,LOW);
+    delay(20);
+    // digitalWrite(Lwrist,HIGH);
+    // delay(200);
+    // digitalWrite(Lwrist,LOW);
+    // delay(20);
+    digitalWrite(Lhand,HIGH);
+    delay(200);
+    digitalWrite(Lhand,LOW);
+    delay(20);
 }
 
 void doAll(){
@@ -110,6 +157,11 @@ void doAll(){
     digitalWrite(elbow2,HIGH);
     digitalWrite(tricep1,HIGH);
     digitalWrite(tricep2,HIGH);
+    digitalWrite(Lelbow,HIGH);
+    digitalWrite(Ltri2,HIGH);
+    digitalWrite(Ltri1,HIGH);
+    digitalWrite(Lwrist,HIGH);
+    digitalWrite(Lhand,HIGH);
     delay(200);
     digitalWrite(hand1,LOW);
     digitalWrite(hand2,LOW);
@@ -117,4 +169,9 @@ void doAll(){
     digitalWrite(tricep2,LOW);
     digitalWrite(elbow1,LOW);
     digitalWrite(elbow2,LOW);
+    digitalWrite(Lelbow,LOW);
+    digitalWrite(Ltri2,LOW);
+    digitalWrite(Ltri1,LOW);
+    digitalWrite(Lwrist,LOW);
+    digitalWrite(Lhand,LOW);
 }

acbdccda643176c6045d0a59c00c15cd5090fa03
psududemike <psududemike@gmail.com>
2015-12-04 01:34:00 -0500

    Update vest-code.c
    Updated with mZone publishing and rest of call of duty video actions

--- vest-code.c
+++ vest-code.c
@@ -12,6 +12,11 @@
 #define rightside2  D4
 #define rightside3  D5
 
+#define ALLSOLENOIDMASK 0x07
+#define SHOULDERMASK 0x10
+#define CREEPYMASK 0x20
+#define SOFTROBOMASK 0x40
+#define HUGMASK 0x80
 
 void setup() {
     Wire.begin();
@@ -38,6 +43,13 @@ void loop() {
 
 }
 
+int readPressure() {
+    int value;
+    Wire.requestFrom(28,sizeof(value));
+    value = Wire.read();
+    return value;
+}
+
 int picker (String command)
 {
     int ind1 = command.indexOf(",");
@@ -59,12 +71,12 @@ int picker (String command)
     } else if (cmd == "shouldermassage") {
         activateShoulders();
     } else if (cmd == "bulletshots") {
-        solenoidShot();
+        bulletshots();
     } else if (cmd == "hugging") {
         hugMe();
     }
     //call of duty actions
-    else if (cmd == "ACT1") {
+    else if (cmd == "Act1") {
         //start vibrators on right
         digitalWrite(rightside1,HIGH);
         digitalWrite(rightside2,HIGH);
@@ -75,7 +87,7 @@ int picker (String command)
         digitalWrite(rightside3,LOW);
         Particle.publish("mZone","ft_rt_ht_vi,bk_rt_vi,2000");
     }
-    else if (cmd=="ACT2") {
+    else if (cmd=="Act2") {
         //left side everything
         digitalWrite(leftside1,HIGH);
         digitalWrite(leftside2,HIGH);
@@ -85,7 +97,9 @@ int picker (String command)
         digitalWrite(leftfront3,HIGH);
         Wire.beginTransmission(32);
         Wire.write(ALLSOLENOIDMASK+SHOULDERMASK);
+        Wire.endTransmission();
         delay(1000);
+        Wire.beginTransmission(32);
         Wire.write(0);
         Wire.endTransmission();
         digitalWrite(leftside1,LOW);
@@ -108,7 +122,122 @@ int picker (String command)
         digitalWrite(leftfront1,LOW);
         digitalWrite(leftfront2,LOW);
         digitalWrite(leftfront3,LOW);
-        Particle.publish("mZone","ft_lt_ht_vi,ft_lt_vt_vi,bk_lt_vi,ft_lt_sl,ft_lt_cu,ft_rt_cu,1600")
+        Particle.publish("mZone","ft_lt_ht_vi,ft_lt_vt_vi,bk_lt_vi,ft_lt_sl,ft_lt_cu,ft_rt_cu,1600");
+    }
+    else if (cmd=="Act4")
+    {
+        //Danger, all vib, sol, cuffs
+        digitalWrite(leftside1,HIGH);
+        digitalWrite(leftside2,HIGH);
+        digitalWrite(leftside3,HIGH);
+        digitalWrite(leftfront1,HIGH);
+        digitalWrite(leftfront2,HIGH);
+        digitalWrite(leftfront3,HIGH);
+        digitalWrite(rightside1,HIGH);
+        digitalWrite(rightside2,HIGH);
+        digitalWrite(rightside3,HIGH);
+        digitalWrite(rightfront1,HIGH);
+        digitalWrite(rightfront2,HIGH);
+        digitalWrite(rightfront3,HIGH);
+        Wire.beginTransmission(32);
+        Wire.write(ALLSOLENOIDMASK+SHOULDERMASK);
+        Wire.endTransmission();
+        Wire.beginTransmission(33);
+        Wire.write(ALLSOLENOIDMASK+SHOULDERMASK);
+        Wire.endTransmission();
+        delay(400);
+        digitalWrite(leftside1,LOW);
+        digitalWrite(leftside2,LOW);
+        digitalWrite(leftside3,LOW);
+        digitalWrite(leftfront1,LOW);
+        digitalWrite(leftfront2,LOW);
+        digitalWrite(leftfront3,LOW);
+        digitalWrite(rightside1,LOW);
+        digitalWrite(rightside2,LOW);
+        digitalWrite(rightside3,LOW);
+        digitalWrite(rightfront1,LOW);
+        digitalWrite(rightfront2,LOW);
+        digitalWrite(rightfront3,LOW);
+        Wire.beginTransmission(32);
+        Wire.write(0);
+        Wire.endTransmission();
+        Wire.beginTransmission(33);
+        Wire.write(0);
+        Wire.endTransmission();
+        Particle.publish("mZone","ft_lt_ht_vi,ft_lt_vt_vi,bk_lt_vi,ft_lt_sl,ft_lt_cu,ft_rt_cu,500");
+        
+    }
+    else if (cmd=="Act5")
+    {
+        //death, all solenoids
+        Wire.beginTransmission(32);
+        Wire.write(ALLSOLENOIDMASK);
+        Wire.endTransmission();
+        Wire.beginTransmission(33);
+        Wire.write(ALLSOLENOIDMASK);
+        Wire.endTransmission();
+        delay(100);
+        Wire.beginTransmission(32);
+        Wire.write(0);
+        Wire.endTransmission();
+        Wire.beginTransmission(33);
+        Wire.write(0);
+        Wire.endTransmission();
+        delay(100);
+        Wire.beginTransmission(32);
+        Wire.write(ALLSOLENOIDMASK);
+        Wire.endTransmission();
+        Wire.beginTransmission(33);
+        Wire.write(ALLSOLENOIDMASK);
+        Wire.endTransmission();
+        delay(100);
+        Wire.beginTransmission(32);
+        Wire.write(0);
+        Wire.endTransmission();
+        Wire.beginTransmission(33);
+        Wire.write(0);
+        Wire.endTransmission();
+        delay(100);
+        Wire.beginTransmission(32);
+        Wire.write(ALLSOLENOIDMASK);
+        Wire.endTransmission();
+        Wire.beginTransmission(33);
+        Wire.write(ALLSOLENOIDMASK);
+        Wire.endTransmission();
+        delay(100);
+        Wire.beginTransmission(32);
+        Wire.write(0);
+        Wire.endTransmission();
+        Wire.beginTransmission(33);
+        Wire.write(0);
+        Wire.endTransmission();
+        delay(100);
+        Particle.publish("mZone","ft_lt_sl,ft_rt_sl,600");
+    }
+    else if (cmd=="Act8")
+    {
+        //Flashbang sensation
+        digitalWrite(leftfront1,HIGH);
+        digitalWrite(leftfront2,HIGH);
+        digitalWrite(leftfront3,HIGH);
+        digitalWrite(rightfront1,HIGH);
+        digitalWrite(rightfront2,HIGH);
+        digitalWrite(rightfront3,HIGH);
+        Wire.beginTransmission(32);
+        Wire.write(SOFTROBOMASK);
+        Wire.endTransmission();
+        
+        delay(300);
+        Wire.beginTransmission(32);
+        Wire.write(0);
+        Wire.endTransmission();
+        digitalWrite(leftfront1,LOW);
+        digitalWrite(leftfront2,LOW);
+        digitalWrite(leftfront3,LOW);
+        digitalWrite(rightfront1,LOW);
+        digitalWrite(rightfront2,LOW);
+        digitalWrite(rightfront3,LOW);
+        Particle.publish("mZone","ft_lt_ht_vi,ft_rt_ht_vi,bk_st_ro,300");
     }
     return 0;
 }
@@ -117,18 +246,24 @@ void doCreepy()
 {
     Wire.beginTransmission(33);
     Wire.write(CREEPYMASK);
+    Wire.endTransmission();
     delay(200);
+    Wire.beginTransmission(33);
     Wire.write(0);
     Wire.endTransmission();
+    Particle.publish("mZone","neck_ar,200");
 }
 
 void activateShoulders()
 {
     Wire.beginTransmission(33);
     Wire.write(SHOULDERMASK);
+    Wire.endTransmission();
     delay(200);
+    Wire.beginTransmission(33);
     Wire.write(0);
     Wire.endTransmission();
+    Particle.publish("mZone","ft_lt_cu,ft_rt_cu,200");
 }
 
 void bulletshots()
@@ -138,7 +273,14 @@ void bulletshots()
 
 void hugMe()
 {
-    
+    Wire.beginTransmission(32);
+    Wire.write(HUGMASK);
+    Wire.endTransmission();
+    delay(3000);
+    Wire.beginTransmission(32);
+    Wire.write(0);
+    Wire.endTransmission();
+    Particle.publish("mZone","ar_ch,3000");
 }
 
 void doTickle()
@@ -168,6 +310,7 @@ void doTickle()
     delay(length);
     digitalWrite(rightside3,LOW);
     delay(shortlength);
+    Particle.publish("mZone","ft_lt_vt_vi,bk_lt_vi,ft_lt_vt_vi,bk_lt_vi,1300");
 }
 
 
